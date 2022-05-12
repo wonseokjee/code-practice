4ac8d444d833bc9e808e6262a40b7786261014f6
demian <wsji9404@gmail.com>
2022-05-12 23:24:14 +0900

    220512 evng solveAc Ex13311 fin

--- main/src/solvedAc/bronze3/Ex13311.java
+++ main/src/solvedAc/bronze3/Ex13311.java
@@ -4,16 +4,11 @@ package solvedAc.bronze3;
 
 public class Ex13311 {
     public static void main(String[] args) {
-
-
-
-        for (int i = 0; i < 10; i++) {
-
-            double a = (Math.random()*1000)+2;
-            long n = (long)((a-1)%a);
-            System.out.println(n);
-        }
-
+        System.out.println(-1);
     }
 
 }
+//2부터 1000까지의 자연수 a에 대하여, n ≡ a-1 (mod a)인 정수 n을 구하여라
+
+// mod는  n을 a로 나누었을 경우 나머지가 a-1이라는 이야기
+// %로 표기
