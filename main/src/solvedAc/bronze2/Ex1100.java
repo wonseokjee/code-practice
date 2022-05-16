750e5149ccecb20effcc8b7fc3720ac633ede958
demian <wsji9404@gmail.com>
2022-05-16 23:07:11 +0900

    20220516

--- main/src/solvedAc/bronze2/Ex1100.java
+++ main/src/solvedAc/bronze2/Ex1100.java
@@ -3,6 +3,7 @@ package solvedAc.bronze2;
 public class Ex1100 {
     public static void main(String[] args) {
         int[][] chess = new int[8][8];
-
+        //행일 짝수일대 0부터 시작 . 홀수일때 1부터 시작
+        //참 거짓? 이나 0,1 로해서 F가 0일때 갯수
     }
 }
