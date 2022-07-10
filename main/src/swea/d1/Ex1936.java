1cfed62b62fc41d83510ecabb1ef0854de7db9c8
demian <wsji9404@gmail.com>
2022-07-10 15:07:16 +0900

    220710 swea ex2058

--- main/src/swea/d1/Ex1936.java
+++ main/src/swea/d1/Ex1936.java
@@ -7,15 +7,16 @@ public class Ex1936 {
         Scanner scan = new Scanner(System.in);
         String i;
         System.out.println("숫자를 두개 입력하시오: ");
-        i = scan.nextLine();
-        String[] num = i.split(" ");
-        int a = Integer.parseInt(num[0]);
-        int b = Integer.parseInt(num[1]);
+//        i = scan.nextLine();
+//        String[] num = i.split(" ");
+//        int a = Integer.parseInt(num[0]);
+//        int b = Integer.parseInt(num[1]);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
         System.out.println(a + b);
         if (a > b) {
             System.out.println(a);
-        }
-        if (a < b) {
+        } else if (a < b) {
             System.out.println(b);
         }
 
--- main/src/swea/d1/Ex2058.java
+++ main/src/swea/d1/Ex2058.java
@@ -1,2 +1,24 @@
-package swea.d1;public class Ex2058 {
+package swea.d1;
+
+import java.util.Scanner;
+
+public class Ex2058 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("자연수를 입력하시오: ");
+        int num = sc.nextInt();
+        if (num<=9999 && num>=1) {
+            String str = String.valueOf(num);
+            String[] arr = str.split("");
+            int b = 0;
+            for (int i = 0; i < arr.length; i++) {
+                b += Integer.parseInt(arr[i]);
+            }
+            System.out.println(b);
+        }else{
+            System.out.println("범위값이 아닙니다.");
+        }
+
+    }
+
 }
