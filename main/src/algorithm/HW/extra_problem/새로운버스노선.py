0a6d385beaa1062a96837e06e672d61cab9b7a09
wonseokjee <wsji9404@gmail.com>
2022-08-19 22:49:38 +0900

    220819 swea

--- main/src/algorithm/HW/extra_problem/새로운버스노선.py
+++ main/src/algorithm/HW/extra_problem/새로운버스노선.py
@@ -1,16 +1,33 @@
-# T = int(input())
-# for tc in range(1,T+1):
-station = [0]*1001
-for n in range(3):
-    num,s,e = map(int,input().split())
-    if num ==1:
+T = int(input())
+for tc in range(1,T+1):
+    station = [0]*1001
+    station_max = 0
+    stop_max = 0
+    N = int(input())
+    for n in range(N):
+        num,s,e = map(int,input().split())
+        stop = 0
+
         for i in range(s, e+1):
             if num == 1:
                 station[i] += 1
+
             if num ==2:
-                if i % 2 ==0:
-                    station[i] += 1
-                elif
+                if s % 2 ==0:
+                    if i % 2 ==0:
+                        station[i] += 1
+                else:
+                    if i % 2 ==1 or i ==1:
+                        station[i] += 1
 
             if num ==3:
-                pass
+                if s % 2 ==0:
+                    if i % 4 ==0:
+                        station[i] += 1
+                else:
+                    if i % 3 == 0 and i % 10 != 1:
+                        station[i] += 1
+        if e > station_max: station_max = e
+        for j in range(station_max+1):
+            if station[j] > stop_max: stop_max = station[j]
+print(f'#{tc} {stop_max}')
