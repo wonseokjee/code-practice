b9dc17653becc64ad8ba72e2e63f3f778b025873
wonseokjee <wsji9404@gmail.com>
2022-09-04 15:14:37 +0900

    220904 ecote

--- main/src/algorithm/greedy/문자열뒤집기.py
+++ main/src/algorithm/greedy/문자열뒤집기.py
@@ -3,19 +3,23 @@ N = list(map(int, input()))
 cnt_0 = 0
 cnt_1 = 0
 k = 0
-for i in range(len(N)):
-    if i ==1:
+for i in range(len(N)): # 0과 1중 개수가 적은 것을 찾기
+    if N[i] == 1:
         cnt_1 += 1
     else:
         cnt_0 += 1
-if cnt_1> cnt_0:
+if cnt_1 >= cnt_0:
     k = 1
 cnt = []
 result = 0
 for j in range(len(N)):
-    if j ==k:
+    if N[j] == k: #개수가 적은 것이면 리스트에 추가
         cnt.append(k)
     else:
-        if cnt != []:
-          result += 1
-          cnt = []
+        if 0 <= j-1 < len(N): #인덱스 에러 방지
+            if N[j-1] != k: #이전숫자가 k가 아니고 같은숫자이면
+                continue
+            else: #이전것이 다른 숫자 이면 리스트 초기화 후 카운트
+                result += 1
+                cnt = []
+print(result)
