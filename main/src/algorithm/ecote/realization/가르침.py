6c48937af4b031365572d374d7b618cdba026d87
wonseokjee <wsji9404@gmail.com>
2022-09-16 11:01:13 +0900

    220916 algostudy

--- main/src/algorithm/ecote/realization/가르침.py
+++ main/src/algorithm/ecote/realization/가르침.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 N, K = map(int, input().split())
 basic = ['a','n','t','i','c']
 result = []
@@ -17,21 +19,30 @@ result_sort = sorted(result)
 
 print(result)
 print(result_sort)
-cnt=1
+cnt= -1
 ans = []
 for j in range(len(result)):
     if j == len(result)-1:
-        if result_sort[j][0] == result_sort[j - 1][0]:
-            result_sort[j][1] = cnt
+        result_sort[j][1] = cnt
         ans.append([len(result_sort[j][0]),result_sort[j][1]])
     elif result_sort[j][0] == result_sort[j+1][0]:
-        cnt += 1
+        cnt += -1
     else:
         result_sort[j][1] = cnt
-        cnt = 1
+        cnt = -1
         ans.append([len(result_sort[j][0]),result_sort[j][1]])
 print(result_sort)
 print(ans)
-
+len_fin = 0
+subsets = [[]]
+subset=[[]]
+for num in ans:
+    size = len(subsets)
+    for y in range(size):
+        subsets.append(subsets[y]+[num])
+        subset[y][0] += num[0]
+        subset[y][1] += num[1]
+print(subsets)
+print(subset)
 
 
