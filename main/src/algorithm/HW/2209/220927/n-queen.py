a3c883a006fff3511c06c316e3c11370b3a733ce
wonseokjee <wsji9404@gmail.com>
2022-09-27 15:47:47 +0900

    220927 HW_ n-queen ing

--- main/src/algorithm/HW/2209/220927/n-queen.py
+++ main/src/algorithm/HW/2209/220927/n-queen.py
@@ -1,44 +1,46 @@
 T = int(input())
 for tc in range(1,T+1):
     N = int(input())
-    visited = [False]*N
-    arr = [[0]*N for _ in range(N)]
     cnt = 0
-    # 2. dfs 정의
-    def dfs(x,y):
+    # 2. 열마다 하나씩 들어가도록 조건 따지기
+    def check(x,y,ins): #
         global cnt
-        if visited[x] == False and arr[x][y] == 0 :
-            visited[x] = True
+        if ins == N:
+            cnt += 1
+        if x<N and y<N:
+            # if visited[y] == False:
+            #     visited[y] = True
+            if correct(x,y):
+                check(x+1,0, ins+1)
+            else:
+                check(x,y+1, ins)
+                arr[x][y] = 0
+
+
+    def correct(x,y): # 위 아래 옆 대각에 문제 없는지 확인
+        for l in all:
+            ni = x + l[0]
+            nj = y + l[1]
+            if 0<= ni <N and 0<= nj < N:
+                if arr[ni][nj] == 1:
+                    # visited[y] = False
+                    return False
+        else:
             arr[x][y] = 1
-            for k in all:
-                ni = x + k[0]
-                nj = y + k[1]
-                if 0 <= ni < N and 0 <= nj < N :
-                    if arr[ni][nj] == 1:
-                        arr[x][y] = 0
-                        visited[x] = False
-                        break
-            if visited[x] == True:
-                for e in range(N): # 다음 열의 행들의 하나하나에 들어가기
-                    ei = x + 1
-                    ej = e
-                    if 0 <= ei <N:
-                        dfs(ei, ej) # dfs
-                        visited[ei] = False
-            if False not in visited:
-                cnt += 1
-            return
+            return True
 
 
     # 1. 행,열 대각 범위 만들기
     all = []
     for a in range(N):
-        all.append([a + 1,0]), all.append([- a - 1, 0]),all.append([0,a + 1]),all.append([0,-a - 1])
-        all.append([a + 1, a + 1]), all.append([- a - 1, - a - 1]),
-        all.append([- a - 1, a + 1]), all.append([a + 1, -a - 1])
+        all.append([a + 1,0]) ,all.append([0,a + 1]),all.append([0,-a - 1]),all.append([-a - 1,0]),
+        all.append([a + 1, a + 1]),all.append([a + 1, -a - 1]),
+        all.append([-a - 1, -a - 1]),all.append([-a - 1,a + 1])
     # print(all)
+
     for h in range(N):
-        dfs(0,h)
-        visited = [False] * N # visited 초기화
+        # visited = [False] * N # visited 초기화
         arr = [[0] * N for _ in range(N)] # 배열 초기화
+        check(0,h,0)
+
     print(f'#{tc} {cnt}')
