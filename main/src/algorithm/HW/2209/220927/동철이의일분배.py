f1ff09b61bba694915d34e8766a3a837322d3613
wonseokjee <wsji9404@gmail.com>
2022-09-27 17:53:53 +0900

    220927 HW

--- main/src/algorithm/HW/2209/220927/동철이의일분배.py
+++ main/src/algorithm/HW/2209/220927/동철이의일분배.py
@@ -1,3 +1,24 @@
+def f(x, cnt):
+    global num
+    if x == N and cnt > num:
+        num = cnt
+        return
+    # if cnt > num:
+    #     num = cnt
+    #     return
+    for i in range(N):
+        if visited[i]:
+            visited[i] = True
+            f(x+1,cnt*arr[x][i])
+            visited[i] = False
+
 T = int(input())
 for tc in range(1,T+1):
-    pass
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    visited = [False]*N
+    num = 1
+    for k in range(N):
+        num = num *arr[k][0]
+    f(0,1)
+    print(num)
