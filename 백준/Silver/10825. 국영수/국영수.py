c86f0acfbac34a961fc6193d7fab62d862ec07cc
wonseokjee <96289688+wonseokjee@users.noreply.github.com>
2022-09-29 23:29:28 +0900

    [Silver IV] Title: 국영수, Time: 4332 ms, Memory: 62880 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,14 @@
+# 국어점수가 높은 순서
+# 같으면 영어점수가 낮은 순서
+# 같으면 수학점수가 높은 순서
+# 모두 같으면 알파벳순서.
+N = int(input())
+arr = []
+for _ in range(N):
+    name, kor, eng, math = input().split()
+    kor, eng, math = int(kor), int(eng), int(math)
+    arr.append([name,kor,eng,math])
+arr.sort(key=lambda x:(-x[1],x[2],-x[3],x[0]))
+# print(arr)
+for i in arr:
+    print(i[0])
