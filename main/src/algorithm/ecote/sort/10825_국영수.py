5d0eb3cea30a15fc0939bb175d799a64ff208f41
wonseokjee <wsji9404@gmail.com>
2022-09-29 23:51:37 +0900

    220929 hw

--- main/src/algorithm/ecote/sort/10825_국영수.py
+++ main/src/algorithm/ecote/sort/10825_국영수.py
@@ -9,4 +9,6 @@ for _ in range(N):
     kor, eng, math = int(kor), int(eng), int(math)
     arr.append([name,kor,eng,math])
 arr.sort(key=lambda x:(-x[1],x[2],-x[3],x[0]))
-print(arr)
+# print(arr)
+for i in arr:
+    print(i[0])
