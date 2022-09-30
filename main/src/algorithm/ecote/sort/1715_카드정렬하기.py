6454941761e57057624ec48c719ea231aad00039
wonseokjee <wsji9404@gmail.com>
2022-09-30 22:19:10 +0900

    220930

--- main/src/algorithm/ecote/sort/1715_카드정렬하기.py
+++ main/src/algorithm/ecote/sort/1715_카드정렬하기.py
@@ -1,15 +1,19 @@
-from collections import deque
-
+import heapq
 
 N = int(input())
-queue = deque([int(input()) for _ in range(N)])
+heap = []
+for _ in range(N):
+    heapq.heappush(heap, int(input()))
+# print(heap)
 cnt = 0
-
-while len(queue)>1:
-    x = queue.popleft()
-    y = queue.popleft()
-    z = x+y
+while heap:
+    x = heapq.heappop(heap)
+    if heap:
+        y = heapq.heappop(heap)
+    else:
+        break
+    z = x + y
+    heapq.heappush(heap,z)
     cnt += z
-    queue.append(z)
-
 print(cnt)
+
