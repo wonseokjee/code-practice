bf2b9632cf75af2d11a00f13d66acc4cd4b23388
wonseokjee <wsji9404@gmail.com>
2024-07-16 13:15:04 +0900

    algo

---
+++ main/src/algorithm/codingTest/1987(알파벳).py
@@ -0,0 +1,24 @@
+import sys
+# sys.setrecursionlimit(20000)
+input = sys.stdin.readline
+R,C = map(int,input().split())
+graph = [input()for _ in range(R)]
+visited = [False for _ in range(26)]
+# print(graph)
+cnt = 0
+d = [(1,0), (-1,0), (0,1), (0,-1)]
+def dfs(x,y,depth):
+    global cnt
+    visited[ord(graph[x][y])-65] = True
+    depth += 1
+    cnt = max(cnt,depth)
+    # print(x,y,depth)
+    for i in range(4):
+        a, b = d[i][0], d[i][1]
+        if 0 <= a+x <R and 0 <= b+y <C:
+            if not visited[ord(graph[a+x][b+y])-65]:
+                # print(a+x, b+y, depth)
+                dfs(a+x,b+y,depth)
+                visited[ord(graph[a+x][b+y])-65] = False
+dfs(0,0,0)
+print(cnt)
