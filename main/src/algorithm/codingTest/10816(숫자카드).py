88fe9563ac5a5f1001d0549b81d367c48300d53e
wonseokjee <wsji9404@gmail.com>
2024-08-17 22:55:53 +0900

    algo

--- main/src/algorithm/codingTest/10816(숫자카드).py
+++ main/src/algorithm/codingTest/10816(숫자카드).py
@@ -3,11 +3,13 @@ cards = list(map(int,input().split()))
 m = int(input())
 num = list(map(int,input().split()))
 dic = {}
+
 for i in cards:
     if i in dic:
         dic[i] += 1
     else:
         dic[i] = 1
+
 for j in num:
     if j in dic:
         print(dic[j],end= ' ')
